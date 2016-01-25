329776068e5117af31c1503b452b7ab2ab1e0d4e
abelhj <abelhj@gmail.com>
2016-01-25 13:51:49 -0600

    clean up stray files

--- scripts/l_bp.py
+++ scripts/l_bp.py
@@ -11,40 +11,6 @@ def find_all(a_str, sub):
         start += len(sub) # use start += 1 to find overlapping matches
 
 
-def parse_vcf_record(vcf_line):
-    header = ''
-    samples = ''
-
-    A = vcf_line.split('\t')
-    if not 'SECONDARY' in A[7]:
-      
-        if 'SVTYPE=BND' in A[7]:
-            m = re.search(r"(\[|\])(.*)(\[|\])",A[4])
-            o_chr, o_pos=m.group(2).split(':')      
-
-            if (o_chr == A[0]) and (('--:' in A[7]) != ('++' in A[7])):
-                neg_s = A[7].find('--:')
-                pos_s = A[7].find('++:')
-
-                if neg_s > 0:
-                    neg_e = neg_s + A[7][neg_s:].find(';')
-                    pre=A[7][:neg_s]
-                    mid=A[7][neg_s:neg_e]
-                    post=A[7][neg_e:]
-                    A[7] = pre + '++:0,' + mid + post
-                else:
-                    pos_e = pos_s + A[7][pos_s:].find(';')
-                    pre=A[7][:pos_s]
-                    mid=A[7][pos_s:pos_e]
-                    post=A[7][pos_e:]
-                    A[7] = pre + mid + ',--:0' + post
-
-                A[7] = 'SVTYPE=INV' + A[7][10:] + ';END=' + o_pos
-                A[4] = '<INV>'
-                vcf_line='\t'.join(A)
-            
-    return vcf_line
-
 
 
 def parse_vcf(vcf_file_name, vcf_lines, vcf_headers, add_sname=True):
@@ -143,11 +109,6 @@ def to_map(s):
             m[A[0]] = None
 
     return m
-
-def vcf_line_key(l1):
-    v1 = split_v(l1)
-    v1[3] = v1[3][:2]
-    return v1
 
 
 def vcf_line_cmp(l1, l2):
