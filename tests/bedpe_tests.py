bd3a2444e10ace2a2ccb1514f0f8984769b5a894
Dave Larson <dlarson@genome.wustl.edu>
2016-02-04 00:06:39 -0600

    additional tests

--- tests/bedpe_tests.py
+++ tests/bedpe_tests.py
@@ -3,7 +3,7 @@ import os
 from svtools.bedpe import Bedpe
 
 class BedpeTests(TestCase):
-    def parse_score(self):
+    def test_parse_score(self):
         self.assertEqual(Bedpe.parse_score('20'), 20)
         self.assertEqual(Bedpe.parse_score('.'), '.')
 
@@ -16,6 +16,18 @@ class BedpeTests(TestCase):
         b2 = Bedpe(entry2)
         self.assertEqual(b2.malformedFlag, 2)
         self.assertEqual(b2.misc[0], entry2[12]) 
+
+    def test_retrieve_svtype(self):
+        entry1 = [ '1', '200', '300', '2', '300', '400', '777_1', '57', '+', '-', 'BND', 'PASS', 'SVTYPE=BND;AF=0.2', 'SVTYPE=BND;AF=0.2' ]
+        b1 = Bedpe(entry1)
+        self.assertEqual(b1.retrieve_svtype(), 'BND')
+
+    def test_retrieve_af(self):
+        entry1 = [ '1', '200', '300', '2', '300', '400', '777_1', '57', '+', '-', 'BND', 'PASS', 'SVTYPE=BND;AF=0.2', 'SVTYPE=BND;AF=0.2' ]
+        b1 = Bedpe(entry1)
+        self.assertEqual(b1.retrieve_af(), '0.2')
+
+
 
 if __name__ == "__main__":
     main()
