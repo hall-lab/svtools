0d2f802ac6fc09a399a0448f806b86d0d094a303
Dave Larson <dlarson@genome.wustl.edu>
2016-02-06 22:27:13 -0600

    whitespace

--- svtools/vcf/genotype.py
+++ svtools/vcf/genotype.py
@@ -17,21 +17,21 @@ class Genotype(object):
                 sys.exit(1)
 
     def set_format(self, field, value):
-        self.set_formats( [field], [value])
+        self.set_formats([field], [value])
         self.variant.update_active_format_list()
 
     def get_format(self, field):
         return self.format[field]
 
     def get_gt_string(self):
-       g_list = list()
-       for f in self.variant.active_format_list:
-           if f in self.format:
-               if type(self.format[f]) == float:
-                   g_list.append('%0.2f' % self.format[f])
-               else:
-                   g_list.append(str(self.format[f]))
-           else:
-               g_list.append('.')
-       return ':'.join(g_list)
+        g_list = list()
+        for f in self.variant.active_format_list:
+            if f in self.format:
+                if type(self.format[f]) == float:
+                    g_list.append('%0.2f' % self.format[f])
+                else:
+                    g_list.append(str(self.format[f]))
+            else:
+                g_list.append('.')
+        return ':'.join(g_list)
 
