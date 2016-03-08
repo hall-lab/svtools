25e78c4e3756b8d22902d6a60d89b87c596a1323
Dave Larson <dlarson@genome.wustl.edu>
2016-03-07 22:52:49 -0600

    remove position adjustments

--- svtools/bedpe.py
+++ svtools/bedpe.py
@@ -31,8 +31,6 @@ class Bedpe(object):
         self.af = self.retrieve_af()
         if self.svtype != bed_list[10]:
             sys.stderr.write("SVTYPE at Column 11({0})) and SVTYPE in INFO Column({1}) don't match at variant ID {3}\n".format(str(bed_list[10]), str(self.svtype), self.name))
-        self.adjust_by_cipos()
-        self.adjust_by_ciend()
 
     @staticmethod
     def parse_score(score):
@@ -61,33 +59,3 @@ class Bedpe(object):
         except IndexError:
             af = None
         return af
-
-    def adjust_by_cipos(self):
-        # CIPOS is adjusted to leftmost before conversion to BEDPE? This removes this adjustment
-        # XXX Do we really want to do this for every single BEDPE line? Or just when converting back to VCF?
-        if 'CIPOS=' in self.info1:
-            self.cipos = re.split('=|,', ''.join(filter(lambda x: 'CIPOS=' in x, self.info1.split(';'))))
-            if self.o1 == '-' and self.svtype == 'BND':
-                self.b1 = self.s1 - int(self.cipos[1]) + 1 
-            else:
-                self.b1 = self.s1 - int(self.cipos[1])
-        else:
-            if self.o1 == '-' and self.svtype == 'BND':
-                self.b1 = self.s1 + 1 
-            else:
-                self.b1 = self.s1
-
-    def adjust_by_ciend(self):
-        if 'CIEND=' in self.info1:     
-            self.ciend = re.split('=|,', ''.join(filter(lambda x: 'CIEND=' in x, self.info1.split(';'))))
-            if self.o2 == '-' and self.svtype == 'BND':
-                self.b2 = self.s2 - int(self.ciend[1]) + 1
-            else:
-                self.b2 = self.s2 - int(self.ciend[1])
-        else: 
-            self.ciend = None
-            if self.o2 == '-' and self.svtype == 'BND':
-                self.b2 = self.s2 + 1
-            else:
-                self.b2 = self.s2
-
