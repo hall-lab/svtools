59023f2369b236be21bf4a51ebc588a4c8ddde65
David Morton <davidlmorton@gmail.com>
2016-03-08 05:56:05 +0000

    vcftobedpe_tests now uses FileConversionBase

--- tests/test_data/vcftobedpe/create_files.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-# This hits files on our filesystem to create our test files.
-
-zcat /gscmnt/gc2802/halllab/sv_aggregate/MISC/lumpy/NA12878/NA12878.sv.vcf.gz | head -1000 > NA12878.vcf
--- tests/vcftobedpe_tests.py
+++ tests/vcftobedpe_tests.py
@@ -1,35 +1,15 @@
-from unittest import TestCase, main
-import os
-import time
-import sys
-import tempfile
-import difflib
+from .file_conversion import FileConversionBase
+from unittest import TestCase
 import svtools.vcftobedpe
 
-class IntegrationTest_vcftobedpe(TestCase):
-    def run_integration_test(self):
-        test_directory = os.path.dirname(os.path.abspath(__file__))
-        test_data_dir = os.path.join(test_directory, 'test_data', 'vcftobedpe')
-        input = os.path.join(test_data_dir, 'NA12878.vcf')
-        expected_result = os.path.join(test_data_dir, 'expected.bedpe')
-        temp_descriptor, temp_output_path = tempfile.mkstemp(suffix='.vcf')
-        with open(input, 'r') as input_handle, os.fdopen(temp_descriptor, 'w') as output_handle:
-            svtools.vcftobedpe.vcfToBedpe(input_handle, output_handle)
+class BedpeToVcfTest(TestCase, FileConversionBase):
+    @property
+    def substitutions(self):
+        return [('#fileDate=.*', '#fileDate=<DUMMY_DATE>')]
 
-            expected_lines = open(expected_result).readlines()
-            # set timestamp for diff
-            expected_lines[1] = '##fileDate=' + time.strftime('%Y%m%d') + '\n'
-            #remove reference line
-            produced_lines = open(temp_output_path).readlines()
-            #produced_lines.pop(2)
-            diff = difflib.unified_diff(produced_lines, expected_lines, fromfile=temp_output_path, tofile=expected_result)
-            result = ''.join(diff)
-            if result != '':
-                for line in result:
-                    sys.stdout.write(line)
-                self.assertFalse(result)
-        os.remove(temp_output_path)
+    def forward_convert(self, input_file, output_file):
+        return svtools.vcftobedpe.vcfToBedpe(input_file, output_file)
 
-if __name__ == "__main__":
-    main()
-
+    @property
+    def test_data_directory_name(self):
+        return 'vcftobedpe'
