54089df5e11310881b231980b1fd9269ff79e642
Dave Larson <dlarson@genome.wustl.edu>
2016-03-07 22:34:08 -0600

    attempt to restructure a little and switch to BPT representation

--- svtools/vcftobedpe.py
+++ svtools/vcftobedpe.py
@@ -7,7 +7,43 @@ import svtools.vcf.file
 import svtools.vcf.variant
 import svtools.utils as su
 
-def writeBND(prim, sec, v, bedpe_out):
+def convert_simple(vcf_line_array, variant):
+    '''
+    Convert simple SVs to BEDPE
+    Precise breakpoints are specified by 0-length intervals.
+    For simple SVs, breakpoint 1 is the VCF coordinate and breakpoint 2 is the END coordinate
+    '''
+    start1 = end1 = variant.pos
+    start2 = end2 = int(variant.info['END'])
+    orientation1 = orientation2 = '+'
+    if 'STRANDS' in variant.info:
+        strands = variant.info['STRANDS']
+        orientation1, orientation2 = strands[:2]
+    if 'CIPOS' in variant.info:
+        span = map(int, variant.info['CIPOS'].split(','))
+        start1 += span[0]
+        end1 += span[1]
+    if 'CIEND' in variant.info:
+        span = map(int, variant.info['CIEND'].split(','))
+        start2 += span[0]
+        start2 += span[1]
+    return '\t'.join(map(str,
+        [variant.chrom,
+        max(start1, 0),
+        max(end1, 0),
+        variant.chrom,
+        max(start2, 0),
+        max(end2, 0),
+        variant.var_id,
+        variant.original_qual,
+        orientation1,
+        orientation2,
+        variant.info['SVTYPE'],
+        variant.filter] +
+        [variant.get_info_string()] + ['.'] + vcf_line_array[8:]
+        ))
+
+def convert_breakend(vcf_line_array, primary_variant, secondary_variant):
     '''
     Parse out strand orientation from BND alt fields
     Simple mapping seems to be left-most strand corresponds to the direction of the square brackets. Right-most to side the reference base is on.
@@ -19,86 +55,76 @@ def writeBND(prim, sec, v, bedpe_out):
     N]2:222222] -> brackets pt right so + and N is on the left so plus ++
     [2:222222[N -> brackets pt left so + and N is on the right so minus --
     '''
-    primary = prim
-    secondary = sec
-    if prim is None:
-        primary = sec 
-    b1 = primary.pos
-    o1 = '+'
-    o2 = '+'
-    sep = ']'
-    if ']' not in primary.alt:
-        sep = '['
-        o2 = '-'
-    if primary.alt.startswith('[') or primary.alt.startswith(']'):
-            o1 = '-'
-    r = re.compile(r'\%s(.+?)\%s' % (sep, sep))
-    chrom2, b2 = r.findall(primary.alt)[0].split(':')
-    b2 = int(b2)
-    primary.set_info('END',b2)
-    # XXX Colby mentioned that coordinates are 0 based sometimes
-    # and not 0-based other times. This appears to be code pertaining to that
-    # If reference is on the right of the brackets, o1 == '-' and coords adjusted
-    if 'CIPOS' in primary.info:
-        span = map(int, primary.info['CIPOS'].split(','))
-        if o1 == '-':
-            b1 -= 1
-        s1 = b1 + span[0]
-        e1 = b1 + span[1]
-    else:
-        if o1 == '-':
-            b1 -= 1
-        else:
-            s1 = b1
-            e1 = b1
+    variant = primary_variant
+    if primary_variant is None:
+        variant = secondary_variant
+    chrom1 = variant.chrom
+    breakpoint1 = variant.pos
+    orientation1 = '+'
+    orientation2 = '+'
+    # NOTE The below is ugly but intended to match things like [2:222[ and capture the brackets
+    r = re.compile(r'([][])(.+?)([][])')
+    sep1, region, sep2 = r.findall(variant.alt)[0]
     
-    if 'CIEND' in primary.info:    
-        span = map(int, primary.info['CIEND'].split(','))
-        if o2 == '-':
-            b2 -= 1
-        s2 = b2 + span[0]
-        e2 = b2 + span[1]
-    else:
-        if o2== '-':
-            b2 -= 1
-        else:
-            s2 = b2
-            e2 = b2
+    assert sep1 == sep2
+    assert sep1 != None
+    
+    chrom2, breakpoint2 = region.split(':')
+    breakpoint2 = int(breakpoint2)
 
-    ispan = s2 - e1
-    ospan = e2 - s1
-    chrom_A = primary.chrom
+    if variant.alt.startswith(sep1):
+        orientation1 = '-'
+        breakpoint1 -= 1
+    if sep1 == '[':
+        orientation2 = '-'
+        breakpoint2 -= 1
+
+    start1 = end1 = breakpoint1
+    start2 = end2 = breakpoint2
+    variant.set_info('END', breakpoint2)
+
+    if 'CIPOS' in variant.info:
+        span = map(int, variant.info['CIPOS'].split(','))
+        start1 += span[0]
+        end1 += span[1]
+    
+    if 'CIEND' in variant.info:    
+        span = map(int, variant.info['CIEND'].split(','))
+        start2 += span[0]
+        end2 += span[1]
+
+    chrom_A = variant.chrom
     chrom_B = chrom2
     
     # write bedpe
     #Swap fields for no primary present as we did calculation with secondary
-    if prim is None:
+    if primary_variant is None:
         info_A = "MISSING"
-        chrom_A = chrom2
-        chrom_B = primary.chrom
-        s1, s2 = s2, s1
-        e1, e2 = e2, e1
-        o1, o2 = o2, o1
+        chrom_A, chrom_B = chrom_B, chrom_A
+        start1, start2 = start2, start1
+        end1, end2 = end2, end1
+        orientation1, orientation2 = orientation2, orientation1
     else:
-        info_A = primary.get_info_string()    
-    if sec is None:
+        info_A = primary_variant.get_info_string()    
+    if secondary_variant is None:
         info_B = "MISSING"
     else:
-        info_B = secondary.get_info_string()    
-    bedpe_out.write('\t'.join(map(str,
+        info_B = secondary_variant.get_info_string()    
+    return '\t'.join(map(str,
                                   [chrom_A,
-                                   max(s1,0) - 1,
-                                   max(e1,0),
+                                   max(start1,0),
+                                   max(end1,0),
                                    chrom_B,
-                                   max(s2,0) - 1,
-                                   max(e2,0),
-                                   primary.info['EVENT'],
-                                   primary.original_qual,
-                                   o1,
-                                   o2,
-                                   primary.info['SVTYPE'],
-                                   primary.filter] + [info_A] + [info_B] + v[8:]
-                                  )) + '\n')
+                                   max(start2,0),
+                                   max(end2,0),
+                                   variant.info['EVENT'],
+                                   variant.original_qual,
+                                   orientation1,
+                                   orientation2,
+                                   variant.info['SVTYPE'],
+                                   variant.filter] + [info_A] + [info_B] + vcf_line_array[8:]
+                                  ))
+
 # primary function
 def vcfToBedpe(vcf_file, bedpe_out):
     vcf = svtools.vcf.file.Vcf()
@@ -164,55 +190,15 @@ def vcfToBedpe(vcf_file, bedpe_out):
         var = svtools.vcf.variant.Variant(v, vcf)
         var.set_info("POS", var.pos)
         if var.info['SVTYPE'] != 'BND':
-            b1 = var.pos
-            b2 = int(var.info['END'])
-            name = v[2]
-            if 'STRANDS' in var.info:
-                strands = var.info['STRANDS']
-                o1 = strands[0]
-                o2 = strands[1]
-            else:
-                o1 = '+'
-                o2 = '+'
-            if 'CIPOS' in var.info:
-                span = map(int, var.info['CIPOS'].split(','))
-                s1 = b1 + span[0]
-                e1 = b1 + span[1]
-            else:
-                e1 = b1
-                s1 = b1
-            if 'CIEND' in var.info:    
-                span = map(int, var.info['CIEND'].split(','))
-                s2 = b2 + span[0]
-                e2 = b2 + span[1]
-            else:
-                e2 = b2
-                s2 = b2    
-
-            ispan = s2 - e1
-            ospan = e2 - s1
-            # write bedpe
-            bedpe_out.write('\t'.join(map(str,
-                                          [var.chrom,
-                                           max(s1,0) - 1,
-                                           max(e1,0),
-                                           var.chrom,
-                                           max(s2,0) - 1,
-                                           max(e2,0),
-                                           name,
-                                           var.original_qual,
-                                           o1,
-                                           o2,
-                                           var.info['SVTYPE'],
-                                           var.filter] +
-                                           [var.get_info_string()] + ['.'] + v[8:]
-                                          )) + '\n')
+            bedpe_out.write(convert_simple(v, var))
+            bedpe_out.write('\n')
         else:
             if 'SECONDARY' in var.info:
                 if var.info['EVENT'] in bnds:
                     #primary
                     var1 = bnds[var.info['EVENT']]
-                    writeBND(var1,var,v,bedpe_out)
+                    bedpe_out.write(convert_breakend(v, var1, var))
+                    bedpe_out.write('\n')
                     del bnds[var.info['EVENT']]                              
                 else:
                     sec_bnds.update({var.info['EVENT']:var})
@@ -221,17 +207,20 @@ def vcfToBedpe(vcf_file, bedpe_out):
                 continue
     intersected_keys = bnds.viewkeys() & sec_bnds.viewkeys()
     for key in intersected_keys:
-       writeBND(bnds[key],sec_bnds[key],v,bedpe_out)
-       del bnds[key] 
-       del sec_bnds[key]
+        bedpe_out.write(convert_breakend(v, bnds[key], sec_bnds[key]))
+        bedpe_out.write('\n')
+        del bnds[key] 
+        del sec_bnds[key]
     if bnds is not None:
         for bnd in bnds:
             sys.stderr.write('Warning: missing secondary multiline variant at ID:' + bnds[bnd].info['EVENT'] + '\n')
-            writeBND(bnds[bnd],None,v,bedpe_out)
+            bedpe_out.write(convert_breakend(v, bnds[bnd], None))
+            bedpe_out.write('\n')
     if sec_bnds is not None:
         for bnd in sec_bnds:
             sys.stderr.write('Warning: missing primary multiline variant at ID:' + sec_bnds[bnd].info['EVENT'] + '\n')
-            writeBND(None,sec_bnds[bnd],v,bedpe_out)
+            bedpe_out.write(convert_breakend(v, None, sec_bnds[bnd]))
+            bedpe_out.write('\n')
             
     # close the files
     bedpe_out.close()
