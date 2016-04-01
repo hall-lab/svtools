2031334e8635a40caeef15d715ccf50b5c52598a
Dave Larson <dlarson@genome.wustl.edu>
2016-03-31 22:59:50 -0500

    start in on tests for converter

---
+++ tests/bedpetovcfconverter_tests.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from svtools.bedpetovcfconverter import BedpeToVcfConverter
+from svtools.vcf.file import Vcf
+from svtools.bedpe import Bedpe
+
+class TestBedpeToVcfConverter(TestCase):
+
+    def setUp(self):
+        vcf = Vcf()
+        self.converter = BedpeToVcfConverter(vcf)
+
+    def test_adjust_by_cipos(self):
+        entry1 = [ '1', '200', '300', '2', '300', '400', '777_1', '57', '+', '-', 'BND', 'PASS', 'SVTYPE=BND;AF=0.2', 'SVTYPE=BND;AF=0.2' ]
+        b1 = Bedpe(entry1)
+        self.assertEqual(self.converter.adjust_by_cipos(b1), 200)
+        b1.o1 = '-'
+        self.assertEqual(self.converter.adjust_by_cipos(b1), 201)
+        b1.info1 = 'SVTYPE=BND;AF=0.2;CIPOS=-2,3'
+        self.assertEqual(self.converter.adjust_by_cipos(b1), 203)
+        b1.o1 = '+'
+        self.assertEqual(self.converter.adjust_by_cipos(b1), 202)
+
+    def test_adjust_by_ciend(self):
+        entry1 = [ '1', '200', '300', '2', '300', '400', '777_1', '57', '+', '-', 'BND', 'PASS', 'SVTYPE=BND;AF=0.2', 'SVTYPE=BND;AF=0.2' ]
+        b1 = Bedpe(entry1)
+        self.assertEqual(self.converter.adjust_by_ciend(b1), 301)
+        b1.o2 = '+'
+        self.assertEqual(self.converter.adjust_by_ciend(b1), 300)
+
+        b1.info1 = 'SVTYPE=BND;AF=0.2;CIEND=-2,3'
+        self.assertEqual(self.converter.adjust_by_ciend(b1), 302)
+        b1.o2 = '-'
+        self.assertEqual(self.converter.adjust_by_ciend(b1), 303)
