d95d1c9363157659d0aa52c155cff93e2c05c81f
Dave Larson <dlarson@genome.wustl.edu>
2016-04-05 16:32:53 -0500

    prune refactor

--- svtools/prune.py
+++ svtools/prune.py
@@ -3,99 +3,98 @@ from svtools.bedpe import Bedpe
 from svtools.cluster import Cluster
 import svtools.utils as su
 
-def prune(cluster_list, bedpe, max_distance, min_cluster_size, print_ineligible, bedpe_out):
-    '''
-    Prints and removes clusters from the cluster_list that are beyond the distance window
-    '''
-    new_cluster_list = []
-    for cluster in cluster_list:
-        # cluster is beyond updatable window:
-        if (bedpe is None or 
-                cluster.chrom_a != bedpe.c1 or 
-                cluster.min_a - max_distance > bedpe.e1 or 
-                cluster.max_a + max_distance < bedpe.s1):
-            
-            # print the cluster if eligible
-            if (cluster.size >= min_cluster_size or print_ineligible):
-                bedpe_out.write(cluster.get_cluster_string() + '\n')
+class Pruner(object):
+    def __init__(self, max_distance, eval_param):
+        self.max_distance = max_distance
+        self.eval_param = eval_param
+        self.min_cluster_size = 1
+        self.bedpe_lines = 0
+        self.skipped_lines = 0
+        self.emitted_lines = 0
+        self.cluster_list = []
+
+    def cluster_bedpe(self, in_file, bedpe_out, is_sorted):
+        # Locally alias instance variables
+        max_distance = self.max_distance
+        eval_param = self.eval_param
+
+        in_header = True
+        for line in in_file:
+            if line.startswith('#') and in_header:
+                bedpe_out.write(line)
+                continue
+            in_header = False
+            self.bedpe_lines += 1
+            bedpe = Bedpe(line.rstrip().split('\t'))
+            if bedpe.af is None:
+                sys.stderr.write('No allele frequency for variant found. This tool requires allele frequency information to function. Please add with svtools afreq and rerun\n')
+                sys.exit(1)
+    
+            matched_clusters = []
+            for cluster in self.cluster_list:
+                if cluster.can_add(bedpe, max_distance):
+                    cluster.add(bedpe, max_distance, eval_param)
+                    matched_clusters.append(cluster)
+            if not matched_clusters:
+                if bedpe.af == '.':
+                    self.skipped_lines += 1
+                else:
+                    new_cluster = Cluster()
+                    new_cluster.add(bedpe, max_distance, eval_param)
+                    self.cluster_list.append(new_cluster)
+            else:
+                if len(matched_clusters) > 1:
+                    i = 0
+                    matched_cluster_pruned = False
+                    while i < (len(matched_clusters) - 1):
+                        j = i + 1
+                        while j < len(matched_clusters):
+                            if matched_clusters[i].can_add(matched_clusters[j].elements[0], max_distance):
+                                matched_clusters[i].add(matched_clusters[j].elements[0], max_distance, eval_param)
+                                matched_cluster_pruned = True
+                                del matched_clusters[j]
+                            j += 1
+                        i += 1        
+                    if matched_cluster_pruned == True:
+                        self.cluster_list = [cluster for cluster in self.cluster_list if cluster not in matched_clusters]
+            #prune and print eligible clusters
+            if self.bedpe_lines % 1000 == 0 and is_sorted:
+                self.cluster_list = self.prune(bedpe,
+                                     False,
+                                     bedpe_out)
+    
+        self.cluster_list = self.prune(None,
+                             True,
+                             bedpe_out)
+
+        return
+
+    def prune(self, bedpe, print_ineligible, bedpe_out):
+        '''
+        Prints and removes clusters from the cluster_list that are beyond the distance window
+        '''
+        min_cluster_size = self.min_cluster_size
+        max_distance = self.max_distance
+        new_cluster_list = []
+        for cluster in self.cluster_list:
+            # cluster is beyond updatable window:
+            if (bedpe is None or 
+                    cluster.chrom_a != bedpe.c1 or 
+                    cluster.min_a - max_distance > bedpe.e1 or 
+                    cluster.max_a + max_distance < bedpe.s1):
+                
+                # print the cluster if eligible
+                if (cluster.size >= min_cluster_size or print_ineligible):
+                    self.emitted_lines += 1
+                    bedpe_out.write(cluster.get_cluster_string() + '\n')
+                else:
+                    new_cluster_list.append(cluster)
+            # cluster is still within updatable window,
+            # leave it in the cluster list
             else:
                 new_cluster_list.append(cluster)
-        # cluster is still within updatable window,
-        # leave it in the cluster list
-        else:
-            new_cluster_list.append(cluster)
-
-    return new_cluster_list
-
-def cluster_bedpe(in_file, max_distance, eval_param, bedpe_out, is_sorted):
     
-    #minimum cluster size to report feature
-    min_cluster_size = 1
-    # line number
-    line_counter = 0
-    # a list of clusters in the buffer
-    cluster_list = []
-    #Flag to print features which are not 
-    #eligible according to min_cluster_size(<2)
-    print_ineligible = 0    
-    #Print headers at top
-    in_header = True
-    for line in in_file:
-        if line.startswith('#') and in_header:
-            bedpe_out.write(line)
-            continue
-        in_header = False
-        line_counter += 1
-        bedpe = Bedpe(line.rstrip().split('\t'))
-        if bedpe.af is None:
-            sys.stderr.write('No allele frequency for variant found. This tool requires allele frequency information to function. Please add with svtools afreq and rerun\n')
-            sys.exit(1)
-
-        matched_clusters = []
-        for cluster in cluster_list:
-            if cluster.can_add(bedpe, max_distance):
-                cluster.add(bedpe, max_distance, eval_param)
-                matched_clusters.append(cluster)
-        if not matched_clusters:
-            new_cluster = Cluster()
-            new_cluster.add(bedpe, max_distance, eval_param)
-            cluster_list.append(new_cluster)
-        else:
-            if len(matched_clusters) > 1:
-                i = 0
-                matched_cluster_pruned = False
-                while i < (len(matched_clusters) - 1):
-                    j = i + 1
-                    while j < len(matched_clusters):
-                        if matched_clusters[i].can_add(matched_clusters[j].elements[0], max_distance):
-                            matched_clusters[i].add(matched_clusters[j].elements[0], max_distance, eval_param)
-                            matched_cluster_pruned = True
-                            del matched_clusters[j]
-                        j += 1
-                    i += 1        
-                if matched_cluster_pruned == True:
-                    cluster_list = [cluster for cluster in cluster_list if cluster not in matched_clusters]
-        #prune and print eligible clusters
-        if line_counter % 1000 == 0 and is_sorted:
-            cluster_list = prune(cluster_list,
-                                 bedpe,
-                                 max_distance,
-                                 min_cluster_size,
-                                 print_ineligible,
-                                 bedpe_out)
-
-    # prune the cluster and print eligible
-    # features
-    ##
-    print_ineligible = True 
-    cluster_list = prune(cluster_list,
-                         None,
-                         max_distance,
-                         min_cluster_size,
-                         print_ineligible,
-                         bedpe_out)
-        
-    return
+        return new_cluster_list
 
 def description():
     return 'cluster and prune a BEDPE file by position based on allele frequency'
@@ -118,11 +117,8 @@ def command_parser():
 
 def run_from_args(args):
     with su.InputStream(args.input) as stream:
-        cluster_bedpe(stream,
-                  args.max_distance,
-                  args.eval_param,
-                  args.output,
-                  args.is_sorted)
+        pruner = Prune(args.max_distance, args.eval_param)
+        pruner.cluster_bedpe(stream, args.output, args.is_sorted)
 
 if __name__ == '__main__':
     parser = command_parser()
