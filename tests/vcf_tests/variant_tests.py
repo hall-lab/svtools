dd0b1b24ae92d76a77bf9975404bbd907997fb9a
Dave Larson <dlarson@genome.wustl.edu>
2016-04-07 21:22:06 -0500

    add test for new get_gt_string() method

--- tests/vcf_tests/variant_tests.py
+++ tests/vcf_tests/variant_tests.py
@@ -13,10 +13,10 @@ class TestVariant(TestCase):
                 '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">',
                 '##FORMAT=<ID=SU,Number=1,Type=Integer,Description="Number of pieces of evidence supporting the variant">',
                 '##FORMAT=<ID=INACTIVE,Number=1,Type=Integer,Description="A format not in use">',
-                '#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	NA12878' ]
+                '#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	NA12878	NA0001' ]
         self.vcf = Vcf()
         self.vcf.add_header(header_lines)
-        self.variant_line = '1	820915	5838_1	N	]GL000232.1:20940]N	0.00	.	SVTYPE=BND;STRANDS=-+:9;IMAFLAG	GT:SU	0/0:9'
+        self.variant_line = '1	820915	5838_1	N	]GL000232.1:20940]N	0.00	.	SVTYPE=BND;STRANDS=-+:9;IMAFLAG	GT:SU	0/0:9	1/1:15'
         self.variant = Variant(self.variant_line.split('\t'), self.vcf)
 
     def test_set_info(self):
@@ -40,6 +40,9 @@ class TestVariant(TestCase):
 
     def test_get_format_string(self):
         self.assertEqual(self.variant.get_format_string(), 'GT:SU') 
+
+    def test_get_gt_string(self):
+        self.assertEqual(self.variant.get_gt_string(), '0/0:9	1/1:15')
 
     def test_genotype(self):
         self.assertEqual(self.variant.genotype('NA12878').get_gt_string(), '0/0:9')
