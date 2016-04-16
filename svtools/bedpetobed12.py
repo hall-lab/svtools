019bb7062371969c58bc74bbc1c0a091d2ca5fd1
Dave Larson <dlarson@genome.wustl.edu>
2016-04-15 22:17:26 -0500

    remove old code

--- svtools/bedpetobed12.py
+++ svtools/bedpetobed12.py
@@ -208,67 +208,6 @@ class BedpetoBlockedBedConverter(object):
                     )
         return output_lines
 
-def bedpeToBlockedBed(bedpe, dist, output_handle=sys.stdout):
-
-    if (bedpe.svtype == "DEL") and (abs(bedpe.e2-bedpe.s1) <= dist): color = "153,0,0"    # deletion breakpoints are red
-    elif (bedpe.svtype == "DUP") and (abs(bedpe.e2-bedpe.s1) <= dist) : color = "0,102,0"  # duplication breakpoints are green
-    elif (bedpe.svtype == "INV") and (abs(bedpe.e2-bedpe.s1) <= dist) : color = "0,51,204"  # inversion breakpoints are blue
-    elif (bedpe.svtype == "BND") or (abs(bedpe.e2-bedpe.s1) > dist): color = "204,204,204" # distant breakpoints are gray
-    elif abs(bedpe.e2-bedpe.s1) <= dist:color="128,0,128" 
-    output_lines = list()
-    if (bedpe.svtype != "BND") and (abs(bedpe.e2-bedpe.s1) <= dist):
-        if bedpe.af is not None:
-            output_lines.append('\t'.join(map(str, [bedpe.c1,bedpe.s1,bedpe.e2,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name,';AF=',bedpe.af])),bedpe.score,'+',\
-                            bedpe.s1,bedpe.e2,color,'2',','.join(map(str,[bedpe.e1-bedpe.s1,bedpe.e2-bedpe.s2])), ','.join(map(str,['0', bedpe.s2 - bedpe.s1]))])))
-        else:
-            output_lines.append('\t'.join(map(str, [bedpe.c1,bedpe.s1,bedpe.e2,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name])),bedpe.score,'+',\
-                            bedpe.s1,bedpe.e2,color,'2',','.join(map(str,[bedpe.e1-bedpe.s1,bedpe.e2-bedpe.s2])), ','.join(map(str,['0', bedpe.s2 - bedpe.s1]))])))
-    # intrachromosomals that exceed dist
-    elif (bedpe.svtype != "BND") and (abs(bedpe.e2-bedpe.s1) > dist):
-        if bedpe.o1 == "+":
-            if bedpe.af is not None:
-                output_lines.append('\t'.join(map(str, [bedpe.c1,bedpe.s1,bedpe.e1+500,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name,';AF=',bedpe.af])),bedpe.score,'+',\
-                                bedpe.s1,bedpe.e1+500,color,'2',','.join(map(str,[bedpe.e1-bedpe.s1,1])), ','.join(map(str,[0, bedpe.e1 - bedpe.s1+500]))])))
-            else:
-                output_lines.append('\t'.join(map(str, [bedpe.c1,bedpe.s1,bedpe.e1+500,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name])),bedpe.score,'+',\
-                                bedpe.s1,bedpe.e1+500,color,'2',','.join(map(str,[bedpe.e1-bedpe.s1,1])), ','.join(map(str,[0, bedpe.e1 - bedpe.s1+500]))])))
-        if bedpe.o1 == "-":
-            if bedpe.af is not None:
-                output_lines.append('\t'.join(map(str, [bedpe.c1,bedpe.s1-500,bedpe.e1,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name,';AF=',bedpe.af])),bedpe.score,'-',\
-                                bedpe.s1-500,bedpe.e1,color,'2',','.join(map(str,[1,bedpe.e1-bedpe.s1])), ','.join(map(str,[0, 500]))])))
-            else:
-                output_lines.append('\t'.join(map(str, [bedpe.c1,bedpe.s1-500,bedpe.e1,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name])),bedpe.score,'-',\
-                                bedpe.s1-500,bedpe.e1,color,'2',','.join(map(str,[1,bedpe.e1-bedpe.s1])), ','.join(map(str,[0, 500]))])))
-        if bedpe.o2 == "+":
-            if bedpe.af is not None:
-                output_lines.append('\t'.join(map(str, [bedpe.c2,bedpe.s2,bedpe.e2+500,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name,';AF=',bedpe.af])),bedpe.score,'+',\
-                                bedpe.s2,bedpe.e2+500,color,'2',','.join(map(str,[bedpe.e2-bedpe.s2,1])), ','.join(map(str,[0, bedpe.e2-bedpe.s2+499]))])))
-            else:
-                output_lines.append('\t'.join(map(str, [bedpe.c2,bedpe.s2,bedpe.e2+500,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name])),bedpe.score,'+',\
-                                bedpe.s2,bedpe.e2+500,color,'2',','.join(map(str,[bedpe.e2-bedpe.s2,1])), ','.join(map(str,[0, bedpe.e2-bedpe.s2+499]))])))
-            
-        if bedpe.o2 == "-":
-            if bedpe.af is not None:
-                output_lines.append('\t'.join(map(str, [bedpe.c2,bedpe.s2-500,bedpe.e2,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name,';AF=',bedpe.af])),bedpe.score,'-',\
-                                bedpe.s2-500,bedpe.e2,color,'2',','.join(map(str,[1,bedpe.e2-bedpe.s2])), ','.join(map(str,[0,500]))])))
-            else:
-                output_lines.append('\t'.join(map(str, [bedpe.c2,bedpe.s2-500,bedpe.e2,''.join(map(str,[bedpe.svtype,';ID=',bedpe.name])),bedpe.score,'-',\
-                                bedpe.s2-500,bedpe.e2,color,'2',','.join(map(str,[1,bedpe.e2-bedpe.s2])), ','.join(map(str,[0,500]))])))
-    # BNDS:	
-    elif (bedpe.svtype == "BND"):
-        if bedpe.af is not None:
-            output_lines.append('\t'.join([bedpe.c1,str(bedpe.s1),str(bedpe.e1),''.join(["BND;ID=",bedpe.name,";AF=",bedpe.af,";STR=",bedpe.o1,bedpe.o2]), \
-                            str(bedpe.score),bedpe.o1,str(bedpe.s1),str(bedpe.e1),"204,204,204"]))
-            output_lines.append('\t'.join([bedpe.c2,str(bedpe.s2),str(bedpe.e2),''.join(["BND;ID=",bedpe.name,";AF=",bedpe.af,";STR=",bedpe.o1,bedpe.o2]),\
-                            str(bedpe.score),bedpe.o2,str(bedpe.s2),str(bedpe.e2),"204,204,204"]))
-        else:
-            output_lines.append('\t'.join([bedpe.c1,str(bedpe.s1),str(bedpe.e1),''.join(["BND;ID=",bedpe.name,";STR=",bedpe.o1,bedpe.o2]),\
-                            str(bedpe.score),bedpe.o1,str(bedpe.s1),str(bedpe.e1),"204,204,204"]))
-            output_lines.append('\t'.join([bedpe.c2,str(bedpe.s2),str(bedpe.e2),''.join(["BND;ID=",bedpe.name,";STR=",bedpe.o1,bedpe.o2]),\
-                            str(bedpe.score),bedpe.o2,str(bedpe.s2),str(bedpe.e2),"204,204,204"]))
-    if len(output_lines) > 0:
-        output_handle.write('\n'.join(output_lines) + '\n')
-            
 def processBEDPE(bedpe_stream, name, dist, output_handle=sys.stdout):
     #Process the BEDPE file and convert each entry to SAM.
     converter = BedpetoBlockedBedConverter(name, dist)
