0932fcda5baf94da5e32db2d22bb2c6a0719fd1a
Dave Larson <dlarson@genome.wustl.edu>
2017-08-30 14:47:46 -0500

    update to version required for latest svtyper

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
 
-    install_requires=['pysam>=0.8.1', 'numpy', 'scipy', 'statsmodels', 'pandas', 'setuptools'],
+    install_requires=['pysam>=0.8.4', 'numpy', 'scipy', 'statsmodels', 'pandas', 'setuptools'],
     scripts=['scripts/create_coordinates'],
     
     entry_points={
