bccd35ee9ef93891e0b30e128df6dc5710c37d5e
Dave Larson <dlarson@genome.wustl.edu>
2017-11-08 14:48:14 -0600

    Ensure vcftobedpe conversion supports 8-column VCFs

--- svtools/vcftobedpeconverter.py
+++ svtools/vcftobedpeconverter.py
@@ -148,7 +148,7 @@ class VcfToBedpeConverter(object):
 
 
 
-        return Bedpe(map(str,[
+        fields = map(str, [
             c1,
             max(s1, 0),
             max(e1, 0),
@@ -169,6 +169,8 @@ class VcfToBedpeConverter(object):
             orig_alt_b,
             info_a,
             info_b,
-            vcf_variant.get_format_string(),
-            vcf_variant.get_gt_string()]))
+            ])
+        if vcf_variant.get_format_string() is not None:
+            fields += [vcf_variant.get_format_string(), vcf_variant.get_gt_string()]
+        return Bedpe(fields)
 
